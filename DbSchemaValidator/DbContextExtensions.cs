1e7a424069e6b1a881dc974083f6d588486d0dd4
Cédric Luthi <cedric.luthi@gmail.com>
2017-11-22 23:34:45 +0100

    Implement the DbContext.ValidateSchema() method

---
+++ DbSchemaValidator/DbContextExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbSchemaValidator
+{
+    public class InvalidMappingException : Exception
+    {
+        public InvalidMappingException(Type entityType, string query, string message, Exception innerException) : base(message, innerException)
+        {
+            EntityType = entityType;
+            Query = query;
+        }
+        
+        public Type EntityType { get; }
+        public string Query { get; }
+    }
+    
+    public static class DbContextExtensions
+    {
+        public static async Task ValidateSchema(this DbContext context)
+        {
+            await context.Database.Connection.OpenAsync();
+
+            var set = typeof(DbContext).GetMethod(typeof(DbSet<>), nameof(DbContext.Set));
+            var take = typeof(Queryable).GetMethod(typeof(IQueryable<>), nameof(Queryable.Take), typeof(IQueryable<>), typeof(int));
+            var toListAsync = typeof(QueryableExtensions).GetMethod(typeof(Task<List<object>>), nameof(QueryableExtensions.ToListAsync), typeof(IQueryable));
+            
+            var contextAssembly = context.GetType().Assembly;
+            foreach (var type in ((IObjectContextAdapter)context).ObjectContext.MetadataWorkspace.GetItems<EntityType>(DataSpace.OSpace))
+            {
+                var entityType = contextAssembly.GetType(type.FullName, throwOnError: true);
+                var dbSet = set.MakeGenericMethod(entityType).Invoke(context, new object[] {});
+                var query = take.MakeGenericMethod(entityType).Invoke(null, new[] {dbSet, 1});
+                try
+                {
+                    // runtime generic version of `await context.Set<entityType>().Take(1).ToListAsync()`
+                    await (Task)toListAsync.Invoke(null, new[] {query});
+                }
+                catch (Exception exception)
+                {
+                    var innerException = (exception as EntityCommandExecutionException)?.InnerException ?? exception;
+                    var message = $"Invalid mapping for DbSet<{entityType?.FullName}> in {context.GetType().FullName}. See the inner exception for details.";
+                    throw new InvalidMappingException(entityType, query.ToString(), message, innerException);
+                }
+            }
+        }
+    }
+}
---
+++ DbSchemaValidator/TypeExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DbSchemaValidator
+{
+    public static class TypeExtensions
+    {
+        private class SimpleTypeComparer : IEqualityComparer<Type>
+        {
+            private static readonly Lazy<SimpleTypeComparer> LazyTypeComparer = new Lazy<SimpleTypeComparer>(() => new SimpleTypeComparer());
+            
+            public static SimpleTypeComparer Instance => LazyTypeComparer.Value;
+            
+            public bool Equals(Type a, Type b)
+            {
+                if (a == null)
+                    throw new ArgumentNullException(nameof(a));
+                if (b == null)
+                    throw new ArgumentNullException(nameof(b));
+                
+                return a.Assembly == b.Assembly &&
+                       a.Namespace == b.Namespace &&
+                       a.Name == b.Name;
+            }
+            
+            public int GetHashCode(Type obj)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        public static MethodInfo GetMethod(this Type type, Type returnType, string name, params Type[] parameterTypes)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+            if (returnType == null)
+                throw new ArgumentNullException(nameof(returnType));
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+            if (parameterTypes == null)
+                throw new ArgumentNullException(nameof(parameterTypes));
+            
+            foreach (var method in type.GetMethods().Where(m => m.Name == name))
+            {
+                var methodParameterTypes = method.GetParameters().Select(p => p.ParameterType);
+                var returnTypeMatch = SimpleTypeComparer.Instance.Equals(returnType, method.ReturnType);
+                var parameterTypesMatch = methodParameterTypes.SequenceEqual(parameterTypes, SimpleTypeComparer.Instance);
+                if (returnTypeMatch && parameterTypesMatch)
+                {
+                    return method;
+                }
+            }
+            throw new MissingMethodException($"Method {name}({string.Join(", ", parameterTypes.Select(p => p.Name))}) was not found.");
+        }
+    }
+}
