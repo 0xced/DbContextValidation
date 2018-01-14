d16ce94a4bcfe2215306e13ea0ba1a20c1d0423c
Cédric Luthi <cedric.luthi@gmail.com>
2018-01-14 21:51:29 +0100

    Document the `InvalidMappingException` class

--- DbSchemaValidator/InvalidMappingException.cs
+++ DbSchemaValidator/InvalidMappingException.cs
@@ -2,6 +2,10 @@
 
 namespace DbSchemaValidator
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// The exception thrown when the context does not match the actual database schema. Has a <exception cref="System.Data.Common.DbException">DbException</exception> inner exception.
+    /// </summary>
     public class InvalidMappingException : Exception
     {
         internal InvalidMappingException(Type entityType, string message, Exception innerException) : base(message, innerException)
@@ -9,6 +13,9 @@ namespace DbSchemaValidator
             EntityType = entityType;
         }
 
+        /// <summary>
+        /// The type of the entity which does not match the actual database schema.
+        /// </summary>
         public Type EntityType { get; }
     }
 }
