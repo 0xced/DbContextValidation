66066cdfe565ab4378de548421b9a061a02bd8ae
Cédric Luthi <cedric.luthi@gmail.com>
2018-08-20 21:53:13 +0200

    Add XML documentation

--- DbSchemaValidator/DbContextExtensions.cs
+++ DbSchemaValidator/DbContextExtensions.cs
@@ -16,6 +16,9 @@ namespace DbSchemaValidator.EFCore
 namespace DbSchemaValidator.EF6
 #endif
 {
+    /// <summary>
+    /// DbContextExtensions is a static class used to hold the <see cref="ValidateSchema"/> method.
+    /// </summary>
     public static partial class DbContextExtensions
     {
         private static IEqualityComparer<string> ColumnNameEqualityComparer(IEqualityComparer<string> columnNameEqualityComparer, bool? caseSensitive)
@@ -26,6 +29,14 @@ namespace DbSchemaValidator.EF6
             return caseSensitive is false ? StringComparer.InvariantCultureIgnoreCase : StringComparer.InvariantCulture;
         }
         
+        /// <summary>
+        /// Validates all the table and column names of entities defined in the model associated with the context against the actual database schema.
+        /// </summary>
+        /// <param name="context">The context you want to validate against its actual database connection.</param>
+        /// <param name="columnNameEqualityComparer">An equality comparer used to compare column names defined in the model against the actual column names. If <code>null</code>, tries to guess from the database provider if table names are case sensitive or not. If case sensitivity can not be guessed, then <code>StringComparer.InvariantCulture</code> is used.</param>
+        /// <param name="progress">A progress reporting numbers between 0.0 and 1.0 representing the completed fraction of the validation process. If <code>null</code>, no progress is reported.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> that you can use to abort the validation process.</param>
+        /// <returns>A collection of <see cref="InvalidMapping"/>s, i.e. when an entity defined in the model does not have a matching table and column names in the database. If the context model exactly matches the database schema then an empty collection is returned.</returns>
         public static async Task<IReadOnlyCollection<InvalidMapping>> ValidateSchema(this DbContext context, IEqualityComparer<string> columnNameEqualityComparer = null, IProgress<float> progress = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             var invalidMappings = new List<InvalidMapping>();
--- DbSchemaValidator/InvalidMapping.cs
+++ DbSchemaValidator/InvalidMapping.cs
@@ -6,6 +6,9 @@ namespace DbSchemaValidator.EFCore
 namespace DbSchemaValidator.EF6
 #endif
 {
+    /// <summary>
+    /// Represents an invalid mapping, i.e. a mapping where the actual table and column names do not match the names defined in the DbContext model.
+    /// </summary>
     public class InvalidMapping
     {
         internal InvalidMapping(string tableName, IReadOnlyCollection<string> missingColumns)
@@ -14,9 +17,18 @@ namespace DbSchemaValidator.EF6
             MissingColumns = missingColumns;
         }
 
+        /// <summary>
+        /// The name of the table.
+        /// </summary>
         public string TableName { get; }
+
+        /// <summary>
+        /// The collection of column names which is defined in the DbContext model but not found in the actual database. If <code>null</code>, the table itself is missing.
+        /// This collection can never be empty. 
+        /// </summary>
         public IReadOnlyCollection<string> MissingColumns { get; }
 
+        /// <returns>A description of the invalid mapping including the table name and all its missing columns.</returns>
         public override string ToString()
         {
             return MissingColumns == null
