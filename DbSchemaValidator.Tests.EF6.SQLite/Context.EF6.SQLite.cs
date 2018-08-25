cff0a9f94cb525495e3ecc7dd4632864c4594dfa
Cédric Luthi <cedric.luthi@gmail.com>
2018-08-25 11:35:12 +0200

    No need to register System.Data.SQLite.EF6
    Actually the invariant name is System.Data.SQLite

--- DbSchemaValidator.Tests.EF6.SQLite/Context.EF6.SQLite.cs
+++ DbSchemaValidator.Tests.EF6.SQLite/Context.EF6.SQLite.cs
@@ -22,9 +22,7 @@ namespace DbSchemaValidator.Tests.EF6.SQLite
         {
             SetDefaultConnectionFactory(new SQLiteConnectionFactory());
             SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
-            SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
             SetProviderServices("System.Data.SQLite", (DbProviderServices)SQLiteProviderFactory.Instance.GetService(typeof(DbProviderServices)));
-            SetProviderServices("System.Data.SQLite.EF6", (DbProviderServices)SQLiteProviderFactory.Instance.GetService(typeof(DbProviderServices)));
         }
     }
 
