1df2b618ba36132f5de78bda470e0c14be28c6b1
Cédric Luthi <cedric.luthi@gmail.com>
2018-09-06 22:28:20 +0200

    Rename DbContextValidation.db into DbContextValidation.sqlite3

--- DbContextValidation.Tests/Configuration/SQLite.cs
+++ DbContextValidation.Tests/Configuration/SQLite.cs
@@ -8,7 +8,7 @@ namespace DbContextValidation.Tests
         public static readonly Provider Provider = Provider.SQLite;
         public static readonly string Schema = null;
 
-        public static readonly string ConnectionString = "Data Source=../../../../DbContextValidation.Tests/DbContextValidation.db";
+        public static readonly string ConnectionString = "Data Source=../../../../DbContextValidation.Tests/DbContextValidation.sqlite3";
 
         public static string DockerArguments(Func<string, string> sqlDirectory)
         {
