3df0a04da2c6092bb12fc0f96a96dbf2fd2d68a6
Cédric Luthi <cedric.luthi@gmail.com>
2019-05-21 10:20:18 +0200

    Make TableNotFoundException serializable

--- DbContextValidation/TableNotFoundException.cs
+++ DbContextValidation/TableNotFoundException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.Common;
+using System.Runtime.Serialization;
 
 #if EFCORE
 namespace DbContextValidation.EFCore
@@ -11,14 +12,37 @@ namespace DbContextValidation.EF6
     /// Represents errors that occur while trying to get the list of column names for a given table.
     /// </summary>
     /// <seealso cref="MissingTableError.MissingTableException"/>
-    public class TableNotFoundException : Exception
+    [Serializable]
+    public sealed class TableNotFoundException : Exception
     {
-        internal TableNotFoundException(string schema, string tableName, DbException dbException, string selectStatement) : base($"{schema}{(string.IsNullOrEmpty(schema) ? "" : ".")}{tableName} not found", dbException)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TableNotFoundException"/> class.
+        /// </summary>
+        /// <param name="schema">The schema of the table.</param>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="dbException">The DbException that was thrown while retrieving the table information.</param>
+        /// <param name="selectStatement">The select statement that was issued to the database that generated the <see cref="DbException"/>.</param>
+        public TableNotFoundException(string schema, string tableName, DbException dbException, string selectStatement) : base($"{schema}{(string.IsNullOrEmpty(schema) ? "" : ".")}{tableName} not found", dbException)
         {
             DbException = dbException;
             SelectStatement = selectStatement;
         }
-        
+
+        /// <inheritdoc />
+        private TableNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            DbException = (DbException)info.GetValue(nameof(DbException), typeof(DbException));
+            SelectStatement = info.GetString(nameof(SelectStatement));
+        }
+
+        /// <inheritdoc />
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue(nameof(DbException), DbException, typeof(DbException));
+            info.AddValue(nameof(SelectStatement), SelectStatement);
+        }
+
         /// <summary>
         /// The DbException that was thrown while executing the <see cref="SelectStatement"/>.
         /// </summary>
