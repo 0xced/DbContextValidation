836fb053c6c5caf2f2b86d2ef3244294b9f75793
Cédric Luthi <cedric.luthi@gmail.com>
2019-11-27 17:33:29 +0100

    Simplify database creation scripts
    Now that we don't reuse containers anymore, we don't need to drop potentially existing tables.

--- tests/SQL.Firebird/1. Drop tOrders.sql
+++
@@ -1,4 +0,0 @@
--- See http://www.firebirdfaq.org/faq69/
-EXECUTE BLOCK AS BEGIN
-IF (EXISTS(SELECT 1 FROM rdb$relations WHERE rdb$relation_name = 'tOrders')) THEN EXECUTE STATEMENT 'DROP TABLE "tOrders";';
-END;
--- tests/SQL.Firebird/2. Drop tCustomers.sql
+++
@@ -1,4 +0,0 @@
--- See http://www.firebirdfaq.org/faq69/
-EXECUTE BLOCK AS BEGIN
-IF (EXISTS(SELECT 1 FROM rdb$relations WHERE rdb$relation_name = 'tCustomers')) THEN EXECUTE STATEMENT 'DROP TABLE "tCustomers";';
-END;
