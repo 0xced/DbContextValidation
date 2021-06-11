a8096b4b1844a107b20940ff05fe71387d3d0614
Cédric Luthi <cedric.luthi@gmail.com>
2021-06-12 01:54:53 +0200

    Fix DbContextValidation.Tests.EF6.SqlServer on net472

--- tests/Config.SqlServer.cs
+++ tests/Config.SqlServer.cs
@@ -1,12 +1,46 @@
-﻿using System.Collections.Generic;
-using DockerRunner.Database.SqlServer;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using DockerRunner;
+#if EF6 || EFCORE2
+using System.Data.SqlClient;
+#else
+using Microsoft.Data.SqlClient;
+#endif
 using static DbContextValidation.Tests.SqlInitializationHelper;
 
 namespace DbContextValidation.Tests
 {
-    public class Configuration : SqlServer2019Configuration
+    public class Configuration : DockerDatabaseContainerConfiguration
     {
         public const string Schema = "dbo";
+
+        private const string User = "sa";
+        // SQL Server password policy requirements: the password must be at least 8 characters long and contain characters from three of the following four sets: Uppercase letters, Lowercase letters, Base 10 digits, and Symbols.
+        private const string Password = "Docker(!)";
+
+        public override string ImageName => "mcr.microsoft.com/mssql/server:2019-latest";
+
+        public override string ConnectionString(string host, ushort port)
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = $"{host},{port}",
+                UserID = User,
+                Password = Password,
+            };
+            return builder.ConnectionString;
+        }
+
+        public override DbProviderFactory ProviderFactory => SqlClientFactory.Instance;
+
+        public override IReadOnlyDictionary<string, string> EnvironmentVariables => new Dictionary<string, string>
+        {
+            ["ACCEPT_EULA"] = "Y",
+            ["SA_PASSWORD"] = Password,
+        };
+
+        public override TimeSpan Timeout => TimeSpan.FromMinutes(1);
 
         public override IEnumerable<string> SqlStatements => ReadSqlStatements(SqlDirectory("SQL.SqlServer"));
     }
