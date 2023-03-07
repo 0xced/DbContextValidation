b9ac57162489373d107ba88e8e86446a50b9f018
Cédric Luthi <cedric.luthi@gmail.com>
2023-03-08 00:51:41 +0100

    Add tests for EF Core 6

--- tests/EFCore5/MySQL.Pomelo/Context.EFCore.MySQL.Pomelo.cs
+++ tests/EFCore5/MySQL.Pomelo/Context.EFCore.MySQL.Pomelo.cs
@@ -1,7 +1,6 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 
-// ReSharper disable once CheckNamespace
 namespace DbContextValidation.Tests.MySQL.Pomelo
 {
     public abstract class Context : DbContext
@@ -15,11 +14,7 @@ namespace DbContextValidation.Tests.MySQL.Pomelo
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-#if EFCORE2
-            optionsBuilder.UseMySql(_connectionString);
-#else
             optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
-#endif
         }
     }
 }
