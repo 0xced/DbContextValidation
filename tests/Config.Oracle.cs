ae123ed42ff7bea517b24ed3ad3511cfbcfc717b
Cédric Luthi <cedric.luthi@gmail.com>
2023-03-08 17:47:54 +0100

    Fix for ORA-01882: timezone region not found
    See https://stackoverflow.com/questions/47469074/timezone-region-not-found/74291427#74291427

--- tests/Config.Oracle.cs
+++ tests/Config.Oracle.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
 using DockerRunner.Database.Oracle;
+using Oracle.ManagedDataAccess.Client;
 using static DbContextValidation.Tests.SqlInitializationHelper;
 
 namespace DbContextValidation.Tests
@@ -9,5 +12,36 @@ namespace DbContextValidation.Tests
         public const string Schema = null;
 
         public override IEnumerable<string> SqlStatements => ReadSqlStatements(SqlDirectory("SQL.Oracle"));
+
+        public override DbProviderFactory ProviderFactory => new OracleProviderFactory(base.ProviderFactory);
+
+        private class OracleProviderFactory : DbProviderFactory
+        {
+            private readonly DbProviderFactory _factory;
+
+            public OracleProviderFactory(DbProviderFactory factory) => _factory = factory;
+
+            public override DbConnection CreateConnection()
+            {
+                var connection = (OracleConnection)_factory.CreateConnection() ?? throw new InvalidOperationException($"CreateConnection() returned null for {_factory}");
+                // Fixes "ORA-01882: timezone region not found" that occurs on GitHub actions
+                // See https://stackoverflow.com/questions/47469074/timezone-region-not-found/74291427#74291427
+                connection.UseHourOffsetForUnsupportedTimezone = true;
+                return connection;
+            }
+
+            public override DbBatch CreateBatch() => _factory.CreateBatch();
+            public override DbBatchCommand CreateBatchCommand() => _factory.CreateBatchCommand();
+            public override DbCommand CreateCommand() => _factory.CreateCommand();
+            public override DbCommandBuilder CreateCommandBuilder() => _factory.CreateCommandBuilder();
+            public override DbConnectionStringBuilder CreateConnectionStringBuilder() => _factory.CreateConnectionStringBuilder();
+            public override DbDataAdapter CreateDataAdapter() => _factory.CreateDataAdapter();
+            public override DbDataSourceEnumerator CreateDataSourceEnumerator() => _factory.CreateDataSourceEnumerator();
+            public override DbParameter CreateParameter() => _factory.CreateParameter();
+            public override bool CanCreateBatch => _factory.CanCreateBatch;
+            public override bool CanCreateCommandBuilder => _factory.CanCreateCommandBuilder;
+            public override bool CanCreateDataAdapter => _factory.CanCreateDataAdapter;
+            public override bool CanCreateDataSourceEnumerator => _factory.CanCreateDataSourceEnumerator;
+        }
     }
 }
