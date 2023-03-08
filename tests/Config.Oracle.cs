998bb779c5e507627a5711023679e773638dd16f
Cédric Luthi <cedric.luthi@gmail.com>
2023-03-08 18:13:16 +0100

    Fix compilation

--- tests/Config.Oracle.cs
+++ tests/Config.Oracle.cs
@@ -26,22 +26,30 @@ namespace DbContextValidation.Tests
                 var connection = (OracleConnection)_factory.CreateConnection() ?? throw new InvalidOperationException($"CreateConnection() returned null for {_factory}");
                 // Fixes "ORA-01882: timezone region not found" that occurs on GitHub actions
                 // See https://stackoverflow.com/questions/47469074/timezone-region-not-found/74291427#74291427
+#if !NETCOREAPP2_1
                 connection.UseHourOffsetForUnsupportedTimezone = true;
+#endif
                 return connection;
             }
 
-            public override DbBatch CreateBatch() => _factory.CreateBatch();
-            public override DbBatchCommand CreateBatchCommand() => _factory.CreateBatchCommand();
             public override DbCommand CreateCommand() => _factory.CreateCommand();
             public override DbCommandBuilder CreateCommandBuilder() => _factory.CreateCommandBuilder();
             public override DbConnectionStringBuilder CreateConnectionStringBuilder() => _factory.CreateConnectionStringBuilder();
             public override DbDataAdapter CreateDataAdapter() => _factory.CreateDataAdapter();
             public override DbDataSourceEnumerator CreateDataSourceEnumerator() => _factory.CreateDataSourceEnumerator();
             public override DbParameter CreateParameter() => _factory.CreateParameter();
-            public override bool CanCreateBatch => _factory.CanCreateBatch;
+            public override bool CanCreateDataSourceEnumerator => _factory.CanCreateDataSourceEnumerator;
+
+#if NETCOREAPP3_0_OR_GREATER
             public override bool CanCreateCommandBuilder => _factory.CanCreateCommandBuilder;
             public override bool CanCreateDataAdapter => _factory.CanCreateDataAdapter;
-            public override bool CanCreateDataSourceEnumerator => _factory.CanCreateDataSourceEnumerator;
+#endif
+
+#if NET6_0_OR_GREATER
+            public override DbBatch CreateBatch() => _factory.CreateBatch();
+            public override DbBatchCommand CreateBatchCommand() => _factory.CreateBatchCommand();
+            public override bool CanCreateBatch => _factory.CanCreateBatch;
+#endif
         }
     }
 }
